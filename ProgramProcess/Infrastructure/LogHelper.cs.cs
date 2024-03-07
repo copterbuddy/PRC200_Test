eba9a8cc6bf33fd71416689bfe0be31e1dad4560
copterbuddy <copterbuddy@gmail.com>
2024-03-07 11:54:47 +0700

    init project

---
+++ ProgramProcess/Infrastructure/LogHelper.cs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramProcess.Infrastructure;
+
+public static class LogHelper
+{
+    public static void LogInfo(string logDetails)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss.SSS")}[INFO] >> {logDetails}");
+    }
+
+    public static void LogDebug(string logDetails)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss.SSS")}[DEBUG] >> {logDetails}");
+    }
+
+    public static void LogError(string logDetails)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss.SSS")}[ERROR] >> {logDetails}");
+    }
+}
---
+++ ProgramProcess/Program.cs
@@ -0,0 +1,42 @@
+﻿using ProgramProcess.Infrastructure;
+
+namespace ProgramProcess
+{
+    internal static class Program
+    {
+        private static Mutex mutex = new(true, "export-estatement-app-noti");
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            LogHelper.LogInfo("Application started. (To terminate this app press Ctrl+C)");
+
+            try
+            {
+                if (mutex.WaitOne(TimeSpan.Zero, true) == false)
+                {
+                    LogHelper.LogInfo("Another instant is running");
+                    return;
+                }
+
+                try
+                {
+                    Startup app = new();
+                    app.StartAsync().Wait();
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.LogError($"Stopped application because of exception : \r\n{ex.Message}");
+                throw;
+            }
+            finally
+            {
+                LogHelper.LogInfo("Application stopped.");
+            }
+        }
+    }
+}
