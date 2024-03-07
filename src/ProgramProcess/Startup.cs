7232c6d4bb5702c4d6e2bc6d44225ad4f708c972
copterbuddy <copterbuddy@gmail.com>
2024-03-07 14:28:47 +0700

    add test LogHelper

---
+++ src/ProgramProcess/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramProcess;
+
+public class Startup
+{
+    private readonly IServiceCollection services;
+
+    internal async Task StartAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ test/ProgramProcessTest/Infrastructure/LogHelperTest.cs
@@ -0,0 +1,67 @@
+﻿using ProgramProcess.Infrastructure;
+using ProgramProcessTest.Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramProcessTest.Infrastructure;
+
+public class LogHelperTest
+{
+    [Fact]
+    public void LogInfo_ShouldWriteInfoLog()
+    {
+        // Arrange
+        var logDetails = "Test log details";
+        var expected1 = $"{DateTime.Now.ToString("yyyyMMdd")}";
+        var expected2 = $"[INFO] >> {logDetails}";
+
+        // Act
+        using ConsoleOutput consoleOutput = new ConsoleOutput();
+        LogHelper.LogInfo(logDetails);
+
+        // Assert
+        Assert.Contains(expected1, consoleOutput.GetOutput());
+        Assert.Contains(expected2, consoleOutput.GetOutput());
+    }
+
+    [Fact]
+    public void LogDebug_ShouldWriteDebugLog()
+    {
+        // Arrange
+        var logDetails = "Test log details";
+        var expected1 = $"{DateTime.Now.ToString("yyyyMMdd")}";
+        var expected2 = $"[DEBUG] >> {logDetails}";
+
+
+        // Act
+        using ConsoleOutput consoleOutput = new ConsoleOutput();
+        LogHelper.LogDebug(logDetails);
+
+        // Assert
+        Assert.Contains(expected1, consoleOutput.GetOutput());
+        Assert.Contains(expected2, consoleOutput.GetOutput());
+    }
+
+    [Fact]
+    public void LogError_ShouldWriteErrorLog()
+    {
+        // Arrange
+        var logDetails = "Test log details";
+        var expected1 = $"{DateTime.Now.ToString("yyyyMMdd")}";
+        var expected2 = $"[ERROR] >> {logDetails}";
+
+
+        // Act
+        using (var consoleOutput = new ConsoleOutput())
+        {
+            LogHelper.LogError(logDetails);
+
+            // Assert
+            Assert.Contains(expected1, consoleOutput.GetOutput());
+            Assert.Contains(expected2, consoleOutput.GetOutput());
+        }
+    }
+}
